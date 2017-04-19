4e5207f12aadebb53d68aca9ee2d9310c2fd75b9
surfertas <tasuku@gmail.com>
2017-04-20 00:12:46 +0900

    updated formatting

--- algorithm-ws/py_obsavoid/scale_expansion_algo.py
+++ algorithm-ws/py_obsavoid/scale_expansion_algo.py
@@ -81,7 +81,7 @@ class ScaleExpansionDetector(object):
     def get_template_coord(self, rbnd, cbnd, keypoint, scale=1):
         # Helper function
         r, c = self.pair2int(keypoint.pt)
-        size = int((keypoint.size * 1.2 / 9 * 20) * scale//2)
+        size = int((keypoint.size * 1.2 / 9 * 20) * scale // 2)
         r0 = np.max([0, r - size])
         r1 = np.min([rbnd, r + size])
         c0 = np.max([0, c - size])
@@ -93,16 +93,16 @@ class ScaleExpansionDetector(object):
         obstacle = []
         for m in self.matches:
             (
-            t1r0, t1r1, t1c0, t1c1
+                t1r0, t1r1, t1c0, t1c1
             ) = self.get_template_coord(rimg, cimg, self.kp2[m.trainIdx])
-            
+
             trntemplate = self.img_prv[t1r0:t1r1, t1c0:t1c1]
             TMmin = np.inf
             smin = 1.0
             for scale in [1.0, 1.1, 1.2, 1.3, 1.4, 1.5]:
                 trntemplate = imresize(trntemplate, scale)
                 (
-                t2r0, t2r1, t2c0, t2c1
+                    t2r0, t2r1, t2c0, t2c1
                 ) = self.get_template_coord(rimg, cimg, self.kp1[m.queryIdx], scale)
 
                 qrytemplate = self.img_cur[t2r0:t2r1, t2c0:t2c1]
@@ -112,26 +112,24 @@ class ScaleExpansionDetector(object):
 
                 if scale == 1.0:
                     TMone = TMscale
-                
+
                 if TMscale < TMmin:
                     TMmin = TMscale
                     smin = scale
-            
+
             # NOTE: Paper uses 0.8 * TMone but that results in zero obstacles.
-            if smin > 1.2 and TMmin < 1.0*TMone:
+            if smin > 1.2 and TMmin < 1.0 * TMone:
                 obstacle.append(m)
-                
+
         self.matches = obstacle
 
     def get_obstacle_position(self):
-        rtmp, ctmp = [],[]
+        rtmp, ctmp = [], []
         for m in self.matches:
             r, c = self.pair2int(self.kp1[m.queryIdx].pt)
             rtmp.append(r)
             ctmp.append(c)
         return int(np.mean(rtmp)), int(np.mean(ctmp))
-        
-        
 
 
 # Helper functions for prototyping
