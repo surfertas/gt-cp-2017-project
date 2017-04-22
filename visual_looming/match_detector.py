82d93b408147cca0a5b0bcdea9161e5aef294908
surfertas <tasuku@gmail.com>
2017-04-22 11:04:36 +0900

    a bit more cleaning up

--- visual_looming/match_detector.py
+++ visual_looming/match_detector.py
@@ -5,24 +5,21 @@ import cv2
 
 class OrbTracker(object):
 
+    """ Used to find and return the matched keypoints of given images. """
+
     def __init__(self):
-        self.matches = None  # type: list of cv2.DMath
-        self.kp1 = None  # type: list of cv2.KeyPoint items
-        self.des1 = None  # type: numpy.ndarray of numpy.uint8 values.
-        self.kp2 = None  # type: list of cv2.KeyPoint items.
-        self.des2 = None  # type: numpy.ndarray of numpy.uint8 values.
+        self.matches = None     # type: list of cv2.DMath
+        self.kp1 = None         # type: list of cv2.KeyPoint items
+        self.des1 = None        # type: numpy.ndarray of numpy.uint8 values.
+        self.kp2 = None         # type: list of cv2.KeyPoint items.
+        self.des2 = None        # type: numpy.ndarray of numpy.uint8 values.
 
         self.orb = cv2.orb = cv2.ORB_create(
             nfeatures=400, scaleFactor=1.2, WTA_K=2, scoreType=cv2.ORB_HARRIS_SCORE,
-                                            patchSize=31, nlevels=8)  # alternate call required on some OpenCV versions
-        # TODO See if higher nlevels value does some good.
-        # TODO see if higher WTA_K does some good.
-        # http: // docs.opencv.org / trunk / db / d95 / classcv_1_1ORB.html  #
-        # details
-        self.kp2_calculated = False  # no need to calulate same again
+                                            patchSize=31, nlevels=8)
+
+        self.kp2_calculated = False  # no need to calculate same again
         self.bruteforce = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
-        # TODO see if knnmatch, FLANN could do some good
-        # http://docs.opencv.org/3.0-beta/doc/py_tutorials/py_feature2d/py_matcher/py_matcher.html#brute-force-matching-with-sift-descriptors-and-ratio-test
 
     def find_matches_between(self, image_1, image_2):
         """ Return the list of matches between two input images.
